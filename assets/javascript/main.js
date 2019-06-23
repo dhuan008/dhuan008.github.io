e83faf279593d322ff2eb81cae178b5b1bba62e6
David Huang <daveshuang@gmail.com>
2019-06-23 13:50:14 -0700

    added scroll to top button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,4 +11,29 @@ $(document).ready(function () {
     $('.tooltipped').tooltip();
     $('.scrollspy').scrollSpy();
 
+    // Scroll back to top
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 100, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').removeClass('scale-out');
+                }
+                else {
+                    $('#back-to-top').addClass('scale-out');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+
+        $('#back-to-top').on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 500);
+        });
+    }
+
 });
