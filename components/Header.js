732a90ea23134258effa7352230fe50b1fe7916d
David <daveshuang@gmail.com>
2019-11-07 11:15:24 -0800

    changed text

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,24 @@
 import PropTypes from 'prop-types';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faGem from '@fortawesome/fontawesome-free-regular/faGem'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faFileCode from '@fortawesome/fontawesome-free-regular/faFileCode';
 
 const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+    <header id="header" style={props.timeout ? { display: 'none' } : {}}>
         <div className="logo">
-            {/*<span className="icon fa-diamond"></span>*/}
-            <FontAwesomeIcon icon={faGem} transform="grow-18" />
+            <FontAwesomeIcon icon={faFileCode} transform="grow-18" />
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>David Huang</h1>
+                <p>Web Developer</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('intro') }}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('projects') }}>Projects</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('skills') }}>Skills</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('contact') }}>Contact</a></li>
             </ul>
         </nav>
     </header>
@@ -31,4 +29,4 @@ Header.propTypes = {
     timeout: PropTypes.bool
 }
 
-export default Header
+export default Header;
